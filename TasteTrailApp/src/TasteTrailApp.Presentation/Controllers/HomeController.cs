3d0b7f102a311c4c22a16604426c2160416d7770
Zeynab Mahammadova <zeynabmahammadov@outlook.com>
2024-07-07 18:15:26 +0400

    Edit Venue

--- TasteTrailApp/src/TasteTrailApp.Presentation/Controllers/HomeController.cs
+++ TasteTrailApp/src/TasteTrailApp.Presentation/Controllers/HomeController.cs
@@ -21,7 +21,7 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult RestourantDetails()
+    public IActionResult venueDetails()
     {
         return View();
     }
