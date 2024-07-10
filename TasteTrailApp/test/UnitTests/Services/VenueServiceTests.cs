d48407c0818c7cc1d5f7c277e3adbc8ba839e803
makedoniaz <jafar.sadig04@gmail.com>
2024-07-10 22:02:17 +0400

    Add Unit tests methods signatures

--- TasteTrailApp/test/UnitTests/Services/VenueServiceTests.cs
+++ TasteTrailApp/test/UnitTests/Services/VenueServiceTests.cs
@@ -8,7 +8,19 @@ public class VenueServiceTests
     private readonly Mock<IVenueRepository> courseRepositoryMock = new();
 
     [Fact]
-    public async Task CreateCourseAsync_CourseFound_DoesntThrowException()
+    public async Task CreateVenueAsync_CreationComplete_DoesntThrowException()
+    {
+
+    }
+
+    [Fact]
+    public async Task CreateVenueAsync_CreationFailed_ThrowsInvalidOperationException()
+    {
+
+    }
+
+    [Fact]
+    public async Task DeleteVenueAsync_DeleteComplete_DoesntThrowException()
     {
 
     }
