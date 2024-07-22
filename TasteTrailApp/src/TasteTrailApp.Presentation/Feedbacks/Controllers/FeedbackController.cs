a6d534d949eb8ed7d21e061db1d64d66573c5dbc
makedoniaz <jafar.sadig04@gmail.com>
2024-07-23 01:16:40 +0400

    Add feedback controller main endpoints

--- TasteTrailApp/src/TasteTrailApp.Presentation/Feedbacks/Controllers/FeedbackController.cs
+++ TasteTrailApp/src/TasteTrailApp.Presentation/Feedbacks/Controllers/FeedbackController.cs
@@ -1,5 +1,6 @@
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using TasteTrailApp.Core.Feedbacks.Commands;
 
 
 namespace TasteTrailApp.Presentation.Feedbacks.Controllers;
@@ -17,5 +18,63 @@ public class FeedbackController : Controller
     public IActionResult Index()
     {
         return View();
+    }
+
+    [HttpPost(Name = "CreateFeedbackApi")]
+    public async Task<IActionResult> Create(CreateFeedbackCommand newFeedback)
+    {
+        try
+        {
+            // var validatorResult = this._validator.Validate(instance: newVenue);
+            // if (!validatorResult.IsValid)
+            // {
+            //     foreach (var error in validatorResult.Errors)
+            //     {
+            //         base.ModelState.AddModelError(key: error.PropertyName, errorMessage: error.ErrorMessage);
+            //     }
+
+            //     return base.View(viewName: "Create");
+            // }
+            newFeedback.VenueId = (int)TempData["VenueId"]!;
+
+            await this.sender.Send(request: newFeedback);
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            return base.StatusCode(statusCode: StatusCodes.Status500InternalServerError, value: ex.Message);
+        }
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Update([FromBody] UpdateFeedbackCommand newFeedback)
+    {
+        try
+        {
+            await this.sender.Send(request: newFeedback);
+            return base.Ok();
+        }
+        catch (System.Exception ex)
+        {
+            return base.StatusCode(statusCode: StatusCodes.Status500InternalServerError, value: ex.Message);
+        }
+    }
+
+    [HttpDelete("{feedbackId:int}")]
+    public async Task<IActionResult> Delete(int feedbackId)
+    {
+        try
+        {
+            var query = new DeleteFeedbackCommand() {
+                Id = feedbackId
+            };
+
+            await this.sender.Send(query);
+            return base.Ok();
+        }
+        catch (Exception ex)
+        {
+            return base.StatusCode(statusCode: StatusCodes.Status500InternalServerError, value: ex.Message);
+        }
     }
 }
